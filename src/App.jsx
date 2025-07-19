0e96ac8d7d0f72cbd0c3f5e2b81d955a67c8344a
Patricia <patricianjuguna084@gmail.com>
2025-07-19 21:47:26 +0300

    Fetch goals from db.json file logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
 function App() {
+  const[goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8002/goals")
+    .then((res) => res.json())
+    .then((data) => {
+      setGoals(data);
+    })
+    .catch((error) => console.error("Error fetching goals... :", error));
+  }, []);
+
+  
 
   return (
     <>
       <h1 class="text-3xl font-bold underline">
         Smart Goal Planner!
-      </h1> 
+      </h1>
     </>
   )
 }
